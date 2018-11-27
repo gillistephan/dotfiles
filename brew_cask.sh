a9ebb0d2f14a6ec14e064489d3616c889534f5f1
gillistephan <gilli.stephan@gmail.com>
2018-11-27 08:47:11 +0100

    init repo

---
+++ brew_cask.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Install graphical apps
+brew cask install gpg-suite
+brew cask install iterm2
+brew cask install google-chrome
+brew cask install visual-studio-code
+brew cask install slack 
+brew cask install 1password
+brew cask install datagrip
+brew cask install spotify
+
+# Clean up
+brew cleanup
